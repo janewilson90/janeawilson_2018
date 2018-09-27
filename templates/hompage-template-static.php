27dbeddc6df7d43284091a4eef1a39cf96a56873
Jane Wilson <janewilson90@users.noreply.github.com>
2018-09-27 19:09:55 +0100

    Homepage header
    started on the header

---
+++ templates/hompage-template-static.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Template Name: Static Home
+*/
+get_header();
+?>
+
+<section class="row">
+<div class="container">
+  <div class="col-8">
+    <h1 id="page-title">Hey, I'm Jane</h1>
+    <h2><span class="fw500">UX Analyst specialising in</span> <span>app optimisation</span></h2>
+  </div>
+  <div class="col-4">
+    <img src="http://placehold.it/250x250" />
+  </div>
+
+</div>
+</section>
+
+
+<?php get_footer(); ?>
+
