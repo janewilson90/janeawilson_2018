2f87631c27ba745ceaf5470006cbc4cfb1d8c2af
Jane Wilson <janewilson90@users.noreply.github.com>
2018-11-01 18:48:22 +0000

    Update inc/template-tags.php
    Updated template tags so the updated times didn't look quite so shit

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -14,7 +14,7 @@ if ( ! function_exists( 'janeawilson_2018_posted_on' ) ) :
 	function janeawilson_2018_posted_on() {
 		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time> | Updated: <time class="updated" datetime="%3$s">%4$s</time>';
 		}
 
 		$time_string = sprintf( $time_string,
